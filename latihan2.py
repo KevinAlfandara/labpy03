79d4fe7695828cafa6f6625be166c13a19b61b0d
KevinAlfandara <alfandara.kevin@gmail.com>
2019-02-18 12:11:03 +0700

    first commit

---
+++ latihan2.py
@@ -0,0 +1,15 @@
+print ('******************************************************')
+print ('*PROGRAM UNTUK MEMENTUKAN BILANGAN DAN BERHENTI KETIKA INPUT ANGKA 0*')
+print ('*Nama	: Stefanus Kevin Alfandara*')
+print ('*NIM	: 311810200*')
+print ('*Kelas	: TI.18.A.1*')
+print ('******************************************************')
+
+b = 0
+while True:
+	angka = int(input("Masukkan Angka: "))
+	b = angka
+	if b == 0:
+		break
+
+print ('============ S E L E S A I ============')
